3f3523fed8cfee5c29926caef4fc4f9b4a3e4c07
Sara <77834808+saranatour1@users.noreply.github.com>
2024-05-06 23:50:30 +0300

    chore(TOD-24): sign up page route (#4)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import Provider from "@/context/Provider";
-
-const inter = Inter({ subsets: ["latin"] });
+import '@fontsource-variable/inter';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="font-inter w-full h-full min-h-screen max-w-full">
         <Provider>
           {children}
         </Provider>
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -11,21 +11,29 @@ const config: Config = {
       colors:{
         "blue-0":"#00939F",
         "white-0":"#F7FAFC",
+        "white-1":"#FAFAFA",
         "gray-0":"#E9EDF5",
         "gray-1":"#212121",
+        "gray-2":"#67728A",
         "black-0":"#171C26",
-        "black-1":"#171923",
+        "black-1":"#171923", // for headings
         "green-0":"#14804A",
         "green-1":"#E1FCEF",
         "red-0":"#E00909",
-        "purple-0":"#6C63FF"
-
+        "purple-0":"#6C63FF",
+        "blue-1":"#CBD5E0",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily:{
+        "inter":['Inter Variable',"sans-serif"]
+      },
+      boxShadow:{
+        "inner-shadow":"0px 2px 0px 0px rgba(231, 235, 238, 0.20) inset"
+      }
     },
   },
   plugins: [],
